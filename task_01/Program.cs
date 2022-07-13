4ddea65354a0959a0546cde298e55ac5fef98c57
Igor Chichinin <marafon4k@gmail.com>
2022-07-13 19:05:22 +0300

    task_01 solution

--- task_01/Program.cs
+++ task_01/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите 3х значное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SecondNumber(int a) {
+    int firstNumber = a / 100; // 4
+    int secondNumber = a % 10; // 6
+    a = a - secondNumber; // 456 - 6 = 450
+    a = a - firstNumber * 100;
+    a = a / 10;
+    return a;
+}
+
+int sum = SecondNumber(number);
+Console.WriteLine(sum);
