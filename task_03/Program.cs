41c9b9f70c59222a7ba87089ab1cd86b189275b0
Igor Chichinin <marafon4k@gmail.com>
2022-07-15 10:21:46 +0300

    task_02 solution 2

--- task_03/Program.cs
+++ task_03/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: Напишите программу, которая принимает на 
+// вход цифру, обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+while (true)
+{
+    Console.WriteLine("Введите цифру");
+    if (int.TryParse(Console.ReadLine(), out var number))
+    {
+        if (number < 1 || number > 7)
+        {
+            Console.WriteLine("Неверный диапазон");
+            continue;
+        }
+
+        Console.WriteLine(number <= 5 ? "Нет" : "Да");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Введи число");
+    }
+}
+
+
+
+
+
+
+
+// int number = 0;
+
+// while (true) {
+//     Console.WriteLine("Введите цифру (день недели): ");
+//     number = Convert.ToInt32(Console.ReadLine());
+//     if (number >= 1 && number <= 7) {
+//         break;
+//     }
+//     if (number < 1 || number > 7) {
+//         Console.WriteLine("Вы ввели недопустимый диапазон!");
+//     }
+// }
+
+// if (number >= 1 && number <= 5) Console.WriteLine("Нет");
+// if (number >= 6 && number <= 7) Console.WriteLine("Да");
